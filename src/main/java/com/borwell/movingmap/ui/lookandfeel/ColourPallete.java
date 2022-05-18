29d9810dbf2266b58c2e82468c80afc28e709a0e
Tony Dixon <tony.dixon@borwell.com>
2022-05-18 15:05:11 +0100

    Refactored and renamed

--- src/main/java/com/borwell/movingmap/ui/lookandfeel/ColourPallete.java
+++ src/main/java/com/borwell/movingmap/ui/lookandfeel/ColourPallete.java
@@ -3,7 +3,7 @@ package com.borwell.movingmap.ui.lookandfeel;
 import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 
-public final class ColorPallete {
+public final class ColourPallete {
     public static ColorUIResource maxAirfieldHeight = new ColorUIResource(new Color(-65536, true));
     public static ColorUIResource maxAerobaticsAirfieldHeight = new ColorUIResource(new Color(-16384, true));
     public static ColorUIResource minAerobaticsAirfieldHeight = new ColorUIResource(new Color(-16732080, true));
